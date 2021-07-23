07085b7af828dc55726be4aaea70eb18e729ddf7
Archie <35892559+ArchieArch1@users.noreply.github.com>
2021-07-23 19:47:17 +0100

    Added objective widget
    Added objective widget, blueprint and scripted.

--- Source/FirstProject/FirstProjectGameModeBase.cpp
+++ Source/FirstProject/FirstProjectGameModeBase.cpp
@@ -2,4 +2,21 @@
 
 
 #include "FirstProjectGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+
+void AFirstProjectGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	if (ObjectiveWidget == nullptr)
+	{
+		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		ObjectiveWidget = CreateWidget<UUserWidget>(PlayerController, ObjectiveWidgetClass);
+	}
+
+	if (ObjectiveWidget)
+	{
+		ObjectiveWidget->AddToViewport();
+	}
+}
 
--- Source/FirstProject/FirstProjectGameModeBase.h
+++ Source/FirstProject/FirstProjectGameModeBase.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Blueprint/UserWidget.h"
 #include "FirstProjectGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class FIRSTPROJECT_API AFirstProjectGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	void StartPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> ObjectiveWidgetClass;
+
+	UUserWidget* ObjectiveWidget = nullptr;
 };
